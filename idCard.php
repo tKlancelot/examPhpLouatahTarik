923da639989dfc971bd84b9c9d1d99a5410c9587
tKlancelot <princesuper42@gmail.com>
2020-07-15 16:41:24 +0200

    modifié

--- idCard.php
+++ idCard.php
@@ -8,14 +8,14 @@ $dev = ['tarik','louatah',33,'dev','examen php cv','12 rue des bierces', '42270 
 
 <div class="row">
     <div class="container card mt-4 p-0" style="width: 22rem;">
-    <div class="card-body">
     <?php 
     for ($i=0; $i < count($dev); $i++){
     ?>
-        <p class="card-text"><?php echo($dev[$i]) ?></p>
-    </div>
-    </div>
+        <div class="card-body">
+            <p class="card-text"><?php echo($dev[$i]) ?></p>
+        </div>
     <?php 
     }
     ?>
+    </div>
 </div>
